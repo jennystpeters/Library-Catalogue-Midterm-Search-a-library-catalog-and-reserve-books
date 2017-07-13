75cbe0b5cf1ca0e8fb177b66e8e781593fcb4db0
jennystpeters <jennystpeters@gmail.com>
2017-07-13 13:13:09 -0400

    Deleted junk files

--- src/CheckoutMain.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by jenny on 7/12/2017.
- */
-public class CheckoutMain {
-    //FIXME: public static void main(String[] args) {
-
-    //}
-}
--- src/SearchMain.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by jenny on 7/12/2017.
- */
-public class SearchMain {
-    //FIXME: public static void main(String[] args) {
-    //}
-}
